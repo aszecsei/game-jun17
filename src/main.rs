87daec3f2024a20390bf82de9ca234f619e3d0ac
Alic Szecsei <aszecsei@gmail.com>
2017-05-25 18:16:19 -0500

    Added SDL2

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate sdl2;
+
+fn main() {
+    // start sdl2
+
+    let ctx = sdl2::init().unwrap();
+    let video_ctx = ctx.video().unwrap();
+    let mut timer = ctx.timer().unwrap();
+
+    // Create a window
+
+    let mut window = match video_ctx.window("eg01", 640, 480).position_centered().opengl().build() {
+        Ok(window) => window,
+        Err(err)   => panic!("failed to create window: {}", err)
+    };
+
+    // Display the window for 3 seconds
+
+    window.show();
+    timer.delay(3000);
+}
