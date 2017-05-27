e103a4e82fb29be49e43c67b48281bf9933b332f
Alic Szecsei <aszecsei@gmail.com>
2017-05-27 15:00:57 -0500

    Start work on adding texture loading

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -2,17 +2,23 @@ extern crate sdl2;
 
 use sdl2::event::{Event,WindowEvent};
 use sdl2::keyboard::Keycode;
+use sdl2::render::{TextureCreator, Texture};
+use sdl2::surface::{Surface};
+
+use std::collections::HashMap;
+use std::path::Path;
 
 pub mod input;
 pub mod graphics;
 
-pub struct Game {
+pub struct Game<'t> {
     pub renderer: sdl2::render::WindowCanvas,
     pub events: sdl2::EventPump,
-    pub screen: graphics::screen::Screen
+    pub screen: graphics::screen::Screen,
+    textures: HashMap<&'static str, Texture<'t>>
 }
 
-impl Game {
+impl<'t> Game<'t> {
     pub fn start(&mut self) {
         // loop until we receive a QuitEvent
         'event : loop {
@@ -44,6 +50,25 @@ impl Game {
 
     pub fn set_screen(&mut self, new_screen: graphics::screen::Screen) {
         self.screen = new_screen;
+    }
+
+    pub fn load_texture(&mut self, texture_creator: &mut TextureCreator<sdl2::video::WindowContext>, name: &'static str, path: &'static str) -> Texture {
+        // Load a surface.
+        // Surfaces live in system RAM, so they aren't ideal for performance.
+        let surface = match Surface::load_bmp(&Path::new(path)) {
+            Ok(surface) => surface,
+            Err(err)    => panic!("failed to load surface: {}", err)
+        };
+
+        // Convert a surface to a texture.
+        // Textures can be used more efficiently by the GPU. (If one is available.)
+        let texture = match texture_creator.create_texture_from_surface(&surface) {
+            Ok(texture) => texture,
+            Err(err)    => panic!("failed to convert surface: {:?}", err)
+        };
+
+        self.textures.insert(name, texture);
+        return texture;
     }
 }
 
@@ -77,6 +102,7 @@ pub fn new(title:&str) -> Game {
     return Game {
         renderer: renderer,
         events: events,
-        screen: graphics::screen::Screen { components: Vec::new() }
+        screen: graphics::screen::Screen { components: Vec::new() },
+        textures: HashMap::new()
     };
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ mod screens;
 fn main() {
     let mut game = engine::new("GAMEGAMEGAME");
 
+    // SECTION: TEXTURE LOADING
+    let texture_creator = game.renderer.texture_creator();
+    // Load all textures here
+    // Using game.load_texture()
+
     // Add your initial screen to the game
     game.set_screen(screens::main_menu::new());
 
